93ff4dc669ca9e46cbba113c05e2565d9555e15b
Kazım Berkant Ersoy <berkantersoy1995@gmail.com>
2022-03-24 14:54:03 +0300

    Add project files.

---
+++ MyNotes.EntityLayer/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.EntityLayer
+{
+    [Table("tblComments")]
+    public class Comment:BaseEntity
+    {
+        [StringLength(300),Required]
+        public string Text { get; set; }
+
+        public virtual Note Note { get; set; }
+    }
+}
---
+++ MyNotes.EntityLayer/Note.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.EntityLayer
+{
+    [Table("tblNotes")]
+    public class Note:BaseEntity
+    {
+        [StringLength(50),Required]
+        public string Title { get; set; }
+        [StringLength(3000),Required]
+        public string text { get; set; }
+        public bool IsDraft { get; set; }
+        public int LikeCount { get; set; }
+        public int CategoryId { get; set; }
+        public virtual MyNotesUser Owner { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public ICollection<Liked> Likes { get; set; }
+        
+        
+        //eager loading:Oluşturacağım nesnede alt bağlamlar var ise hepsini getirir
+        
+        //Öğrenciler ve sınıflar tablom var bunların arasında foreign key bağlantım var
+        //Öğrenciler Listesi alıdğımda eğer eager loading kullanarak alıyorsam
+        /*
+         *ad
+         * soyad
+         * sinif
+         *          {
+         *              sinifadi
+         *              kat
+         *              eğitmen
+         *           {
+         * Hepsi gelir 
+         *
+         *
+         *
+         *
+         */
+        
+        //Lazy loading: Oluşturulan nesnede getirilir ilgili alt bağlamlar yüklenmez 
+        //Virtual yaptığımız zaman 
+    }
+}
